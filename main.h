2623a52903f3ce8bb2e92a083acf41805d5d0247
kibejay <kibejay61@gmail.com>
2022-04-20 02:17:26 -0700

    main.h4

--- main.h
+++ main.h
@@ -19,3 +19,12 @@ typedef struct sp
 	char sp;
 	int (*f)(va_list, char *, int);
 } sp_t;
+
+int _printf(const char *format, ...);
+int (*get_sp_func(char c))(va_list, char *, int);
+int add_char(va_list ap, char *buffer, int i);
+int add_str(va_list ap, char *buffer, int i);
+int add_str_print(va_list ap, char *buffer, int i);
+int copy_to_buff(char *s, char *buffer, int i);
+int add_percent(va_list ap, char *buffer, int i);
+char *int_to_str(unsigned long int num, int base);
